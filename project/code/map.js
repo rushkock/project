c20cf6264e4001e92848f18979bbe2f40d34a02f
Ruchella Kock <ruchella_kock@hotmail.com>
2019-01-10 12:27:45 +0100

    juiste link in map.js naar data

--- project/code/map.js
+++ project/code/map.js
@@ -7,7 +7,7 @@
 
 window.onload = function()
 {
-  var requests = [d3.json("../data/json/world_countries.json"), d3.tsv("../data/world_population.tsv"), d3.json("../data/json/suicide_pooled.json")];
+  var requests = [d3.json("../data/json/world_countries.json"), d3.tsv("../data/json/world_population.tsv"), d3.json("../data/json/suicide_pooled.json")];
 
   Promise.all(requests).then(function(response) {
     console.log(response[2])
